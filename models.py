23544a512b31c4ccb60076b3dd0e8e120e6a91ba
Naren1122 <https://github.com/Naren1122>
2026-08-31 20:29:59 +0545

    cardiovascular backend

---
+++ models.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+df = pd.read_csv('Cardiovascular_Disease.csv')
+
+age_data = (df['age']/365).astype(int)
+df['age'] = age_data
+
+data_filter = df[(df['height'].between(125,200)) & (df['weight'].between(40,120)) & (df['ap_hi'].between(100,200)) & (df['ap_lo'].between(50,90))]
+df = data_filter
+
+@st.cache_resource
+def logistic_cardio():
+    feature = ['age','gender','height','weight','ap_hi','ap_lo','cholesterol','gluc','smoke','alco','active']
+    target = 'cardio'
+    
+    X = df[feature]
+    Y = df[target]
+    
+    X_train, X_test, Y_train, Y_test = train_test_split(
+    X,Y,test_size=0.2, random_state=42,  stratify = Y
+)
+    
+    scaler = StandardScaler()
+    X_train_scale = scaler.fit_transform(X_train)
+    X_test_scale = scaler.transform(X_test)
+    
+    model = LogisticRegression(
+    solver = 'lbfgs',
+    class_weight = 'balanced',
+    random_state = 42
+)
+    
+    model.fit(X_train_scale,Y_train)
+    Y_pred = model.predict(X_test_scale)
+    
+    cr = classification_report(Y_test, Y_pred , output_dict=True)
+    cm = confusion_matrix(Y_test , Y_pred)
+    
+    return feature, scaler, model, Y_pred, cr, cm
+
+@st.cache_resource
+def svm_cardio(): 
+     features = ['age' , 'gender' , 'height' , 'weight', 'ap_hi', 'ap_lo' , 'cholesterol', 'gluc', 'smoke' , 'alco' , 'active']
+     target = 'cardio'  
+     
+     sampled_df = df.sample(n=5000, random_state=42)
+     
+     X = sampled_df[features]
+     Y = sampled_df[target]
+     
+     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state = 42, stratify = Y)
+     
+     scaler = StandardScaler()
+     X_train_scale = scaler.fit_transform(X_train)
+     X_test_scale = scaler.transform(X_test)
+     
+     model = SVC()
+     model.fit(X_train_scale,Y_train)
+     Y_pred = model.predict(X_test_scale)
+     
+     cr = classification_report(Y_test, Y_pred, output_dict=True)
+     cm = confusion_matrix(Y_test, Y_pred)   
+     
+     return features, scaler, model, Y_pred, cr, cm
